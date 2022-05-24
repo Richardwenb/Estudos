c4c755757950da81ae2991d4bf9bd9bbabc9af76
Richard Wendel <richardwenb@gmail.com>
2022-05-24 14:54:36 -0300

    Create Teste.r

---
+++ Linguagem R/Teste.r
@@ -0,0 +1,7 @@
+#Data Frame
+
+#Definir area de trabalho
+setwd("C:/Users/richard.batista/Desktop/Estudo/Planilha_Marilan") #Definição de diretório que esta trabalhando # nolint
+
+#Importando base de dados
+df <- read.csv("Biscoitos.csv") #importando arquivo csv
