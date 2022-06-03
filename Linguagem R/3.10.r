55708e767af096ff3384e64e050883a04ab20091
Richard Wendel <richardwenb@gmail.com>
2022-06-03 08:11:43 -0300

    Update 3.10.r

--- Linguagem R/3.10.r
+++ Linguagem R/3.10.r
@@ -10,3 +10,20 @@ df2
 mean(df2)
 
 elevado <- (df2 ^ 2)
+
+x <- 4
+if(x == 4) {
+  TRUE
+}
+
+a = 10
+
+b = 20
+
+c = 30
+
+
+
+ifelse( a < b, print('A é menor que B'))
+ifelse( a > b, print('A é maior que B'))
+ifelse( b < a, print('B é menor que A'))
