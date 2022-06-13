81616b6645851e0e8cd63f34762796de738aa169
Richard Wendel <richardwenb@gmail.com>
2022-06-13 14:37:44 -0300

    sistema_terminado

--- Python/banco.py
+++ Python/banco.py
@@ -17,3 +17,17 @@ class Banco():
                 self.clientes[i].getConta().depositar(valor)
                 return True
         return False
+
+    def sacar(self, nome, valor) -> bool:
+        for i in range(len(self.clientes)):
+            if self.clientes[i].getNome() == nome:
+                return self.clientes[i].getConta().sacar(valor)
+        return False
+
+    def transferir(self, nome, nome2, valor) -> bool:
+        for i in range(len(self.clientes)):
+            if self.clientes[i].getNome() == nome:
+                for j in range(len(self.clientes)):
+                    if self.clientes[j].getNome() == nome2:
+                        return self.clientes[i].getConta().transferir(valor, self.clientes[j].getConta())
+        return False
--- Python/main.py
+++ Python/main.py
@@ -5,7 +5,8 @@ banco = Banco()
 
 cont = 1000
 
-banco.cadastrar(Cliente("Nathan","5004564004","Vila lobos", "014669852365", Conta(cont, "corrente", 0)))
+#banco.cadastrar(Cliente("Nathan","5004564004","Vila lobos", "014669852365", Conta(cont, "corrente", 200)))
+#banco.cadastrar(Cliente("Richard","5004564004","Vila lobos", "014669852365", Conta(cont+1, "corrente", 100)))
 
 while True:
     print("MENU:\n1 – Cadastrar cliente/conta\n2 – Consultar\n3 – Depositar\n4 - Sacar\n5 – Transferir\n6 - Finalizar")
@@ -22,8 +23,12 @@ while True:
         endereco = input("Digite o endereço: ")
         telefone = input("Digite o telefone: ")
         tipo = input("\n1 - Corrente\n2 - Poupança\nTipo de conta: ")
-        saldo = input("Depósito inicial: ")
-        
+        while True:
+            try:
+                saldo = float(input("Depósito inicial: "))
+                break
+            except ValueError:
+                print("Valor inválido, use ponto em vez de vírgula\n")
         banco.cadastrar(Cliente(nome, cpf, endereco, telefone, Conta(cont+1, tipo, saldo)))
         cont = cont + 1
         print("Cliente cadastrado com sucesso")
@@ -32,25 +37,47 @@ while True:
         nome = input("Digite o Nome que deseja consultar: ")
         if banco.consultar(nome) != None:
             cliente = banco.consultar(nome)
-            print("Nome: ",cliente.getNome())
+            print("\nNome: ",cliente.getNome())
             print("CPF:",cliente.getCpf())
             print("Endereço:",cliente.getEndereco())
             print("Telefone:",cliente.getTelefone())
             print("Conta:",cliente.getConta().getNumero())
+            print("Saldo: R$%0.2f" %cliente.getConta().getSaldo())
         else:
             print("Cliente inexistente")
 
     elif opc == 3:
         nome = input("Digite o Nome que deseja depositar: ")
         if banco.consultar(nome) != None:
-            valor = input("Valor de depósito: ")
+            valor = float(input("Valor de depósito: "))
             banco.depositar(nome, valor)
         else:
             print("Cliente não encontrado")
-    
+
+    elif opc == 4:
+        nome = input("Digite o Nome que deseja Sacar: ")
+        if banco.consultar(nome) != None:
+            valor = float(input("Digite o valor que deseja sacar: "))
+            if banco.sacar(nome, valor):
+                print("Saque efetuado com sucesso")
+            else:
+                print("Saldo insuficiente")
+        else:
+            print("Cliente não encontrado")
+
+    elif opc == 5:
+        nome = input("Digite o Nome quem irá transferir: ")
+        if banco.consultar(nome) != None:
+            nome2 = input("Digite o nome do cliente quem irá Receber: ")
+            valor = float(input("Digite o valor que deseja transferir: "))
+            if banco.transferir(nome, nome2, valor):
+                print("Transferência efetuada com sucesso")
+            else:
+                print("Saldo insuficiente")
+        else:
+            print("Cliente não encontrado")
+            
     elif opc == 6:
         exit()
 
     print("")
-
-
