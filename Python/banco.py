9ea9106f4722249da4f7f144956cc5f591515f36
Richard Wendel <richardwenb@gmail.com>
2022-06-13 12:29:04 -0300

    Update banco.py

--- Python/banco.py
+++ Python/banco.py
@@ -1,8 +1,19 @@
-from cliente import Cliente
-
 class Banco():
     def __init__(self) -> None:
         self.clientes = []
 
-    def cadastrar(c) -> None:
+    def cadastrar(self, c) -> None:
         self.clientes.append(c)
+
+    def consultar(self, nome) -> object:
+        for i in range(len(self.clientes)):
+            if self.clientes[i].getNome() == nome:
+                return self.clientes[i]
+        return None
+
+    def depositar(self, nome, valor) -> bool:
+        for i in range(len(self.clientes)):
+            if self.clientes[i].getNome() == nome:
+                self.clientes[i].getConta().depositar(valor)
+                return True
+        return False
